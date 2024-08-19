044d219e3109d617755da8857546e9af58e6a65f
JM-Flores <jmiguelflores19@gmail.com>
2024-08-19 17:36:10 +0800

    Add weather loading state

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
 interface Condition {
     code: number;
@@ -19,29 +20,35 @@ interface FetchWeatherResponse {
 
 const useWeather = (city: string | null) => {
     const [weather, setWeather] = useState<CurrentWeather>({} as CurrentWeather);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState("");
     const [isloading, setLoading] = useState(false);
 
-    // setLoading(true);
     useEffect(() => {
+        const controller = new AbortController();
+        
         if (city) {
-          apiClient
+            setLoading(true);
+            apiClient
             .get<FetchWeatherResponse>("/current.json", {
               params: {
                 q: city,
                 aqi: "no",
               },
+              signal: controller.signal
             })
             .then((res) => {
               setWeather(res.data.current);
-            //   setLoading(false);
+              setLoading(false);
             })
             .catch((err) => {
+                if (err instanceof CanceledError) return;
                 setError(err.message)
-                // setLoading(false);
+                setLoading(false);
             });
 
         }
+
+        return () => controller.abort();
       }, [city]);
 
     return {weather, error, isloading};
