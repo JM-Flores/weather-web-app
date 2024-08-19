ac5ee6e9fe69ec1af638b2157df753fb54c6329f
JM-Flores <jmiguelflores19@gmail.com>
2024-08-19 18:12:25 +0800

    Add weather condition and Icon

--- src/components/CurrentWeatherCard.tsx
+++ src/components/CurrentWeatherCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, Heading, Text } from "@chakra-ui/react";
+import { Card, CardHeader, Heading, Image, Text } from "@chakra-ui/react";
 import useWeather from "../hooks/useWeather";
 
 interface Props {
@@ -8,6 +8,11 @@ interface Props {
 const CurrentWeatherCard = ({ city }: Props) => {
   const { weather, error, isloading } = useWeather(city);
 
+  const temperatureC = weather?.temp_c;
+  const temperatureF = weather?.temp_f;
+  const condition = weather?.condition?.text;
+  const conditionIcon = weather?.condition?.icon;
+
   if (!city) return null;
   if (error) return <Text color={"red"}>{error}</Text>;
 
@@ -16,7 +21,9 @@ const CurrentWeatherCard = ({ city }: Props) => {
       <Card>
         <CardHeader>
           <Heading size="md">Current Weather</Heading>
-          <Text>{weather.temp_c}&deg;c</Text>
+          <Text>{temperatureC}&deg;c</Text>
+          <Image src={conditionIcon} alt="" />
+          <Text>{condition}</Text>
         </CardHeader>
       </Card>
     </>
